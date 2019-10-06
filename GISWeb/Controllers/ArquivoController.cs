305f6bb13c9d7f3c710dd9798063ef2435c1e620
Johnnathan Almeida <johnnathanalmeida22@gmail.com>
2019-10-06 10:54:38 -0300

    Business de arquivo.

---
+++ GISWeb/Controllers/ArquivoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GISWeb.Controllers
+{
+    public class ArquivoController : Controller
+    {
+        // GET: Arquivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
