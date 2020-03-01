5fa5049e89c945a884c9f7701c314775bcf80e14
Andre Sombra <andresombra.net@gmail.com>
2020-03-01 12:48:47 -0300

    Novas implementacoes
    Novas implementacoes para deixar tela do GED com responsabilidades independentes da tela do Perfil

--- GISCore/Business/Concrete/AdmissaoBusiness.cs
+++ GISCore/Business/Concrete/AdmissaoBusiness.cs
@@ -1,6 +1,7 @@
 ﻿using GISCore.Business.Abstract;
 using GISModel.DTO.Admissao;
 using GISModel.Entidades;
+using Ninject;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,7 +13,12 @@ namespace GISCore.Business.Concrete
    
     public class AdmissaoBusiness : BaseBusiness<Admissao>, IAdmissaoBusiness
     {
-        
+        [Inject]
+        public IArquivoBusiness ArquivoBusiness { get; set; }
+
+        [Inject]
+        public IREL_ArquivoEmpregadoBusiness REL_ArquivoEmpregadoBusiness { get; set; }
+
         public override void Alterar(Admissao pAdmissao)
         {
             Admissao tempAdmissao = Consulta.FirstOrDefault(p => p.ID.Equals(pAdmissao.ID));
@@ -198,6 +204,226 @@ namespace GISCore.Business.Concrete
                 if (al != null)
                 {
                     lista.Add(al);
+                }
+            }
+
+            return lista;
+        }
+
+        public List<Admissao> BuscarAdmissoesAtuais(string UKEmpregado)
+        {
+            try
+            {
+                List<Admissao> lista = new List<Admissao>();
+
+                string query = @"select a.UniqueKey, a.DataAdmissao, a.DataDemissao, a.Justificativa, e.NomeFantasia as Empresa, u.Nome as NomeUsuario
+                             from tbAdmissao a, tbEmpresa e, tbUsuario u
+                             where a.UKEmpregado = '" + UKEmpregado + @"' and a.Status = 1 and a.UsuarioExclusao is null and
+	                               a.UKEmpresa = e.UniqueKey and e.UsuarioExclusao is null and
+	                               a.UsuarioInclusao = u.Login and u.UsuarioExclusao is null";
+
+                DataTable result = GetDataTable(query);
+                if (result.Rows.Count > 0)
+                {
+                    foreach (DataRow row in result.Rows)
+                    {
+                        Admissao adm = new Admissao()
+                        {
+                            UniqueKey = Guid.Parse(row["UniqueKey"].ToString()),
+                            UKEmpregado = Guid.Parse(UKEmpregado.ToString()),
+                            DataAdmissao = row["DataAdmissao"].ToString(),
+                            DataDemissao = row["DataDemissao"].ToString(),
+                            Justificativa = row["Justificativa"].ToString(),
+                            Empresa = new Empresa()
+                            {
+                                NomeFantasia = row["Empresa"].ToString()
+                            },
+                            UsuarioInclusao = row["NomeUsuario"].ToString(),
+                            Alocacoes = BuscarAlocacoes(row["UniqueKey"].ToString(), Guid.Parse(UKEmpregado.ToString()))
+                        };
+
+                        lista.Add(adm);
+                    }
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro: "+ex.Message);
+            }
+        }
+
+        public List<Alocacao> BuscarAlocacoes(string UKAdmissao, Guid UKEmpregado)
+        {
+
+            List<Alocacao> lista = new List<Alocacao>();
+
+            string query = @"select al.UniqueKey, c.Numero as Contrato, cargo.NomeDoCargo, func.NomeDaFuncao, est.Descricao as Estabelecimento, eq.NomeDaEquipe, dep.Sigla, 
+                                    atv.Descricao as Atividade, al.DataInclusao, al.UsuarioInclusao, est.UniqueKey as UKEstab, func.UniqueKey as UKFuncao
+                             from tbAlocacao al 
+		                             inner join tbContrato c on al.UKContrato = c.UniqueKey and c.UsuarioExclusao is null
+		                             inner join tbCargo cargo on al.UKCargo = cargo.UniqueKey and cargo.UsuarioExclusao is null
+		                             inner join tbFuncao func on al.UKFuncao = func.UniqueKey and func.UsuarioExclusao is null
+		                             inner join tbEstabelecimento est on al.UKEstabelecimento = est.UniqueKey and est.UsuarioExclusao is null
+		                             inner join tbEquipe eq on al.UKEquipe = eq.UniqueKey and eq.UsuarioExclusao is null
+		                             inner join tbDepartamento dep on al.UKDepartamento = dep.UniqueKey and dep.UsuarioExclusao is null
+		                             left outer join REL_FuncaoAtividade fa on func.UniqueKey = fa.UKFuncao and fa.UsuarioExclusao is null
+		                             left outer join tbAtividade atv on fa.UKAtividade = atv.UniqueKey and atv.UsuarioExclusao is null
+                             where al.UsuarioExclusao is null and al.UKAdmissao = '" + UKAdmissao + "' ";
+
+            DataTable result = GetDataTable(query);
+            if (result.Rows.Count > 0)
+            {
+                Alocacao al = null;
+
+                foreach (DataRow row in result.Rows)
+                {
+
+                    if (al == null)
+                    {
+                        al = new Alocacao()
+                        {
+                            UniqueKey = Guid.Parse(row["UniqueKey"].ToString()),
+                            DataInclusao = DateTime.Parse(row["DataInclusao"].ToString()),
+                            UsuarioInclusao = row["UsuarioInclusao"].ToString(),
+                            Contrato = new Contrato()
+                            {
+                                Numero = row["Contrato"].ToString()
+                            },
+                            Cargo = new Cargo()
+                            {
+                                NomeDoCargo = row["NomeDoCargo"].ToString()
+                            },
+                            Funcao = new Funcao()
+                            {
+                                UniqueKey = Guid.Parse(row["UKFuncao"].ToString()),
+                                NomeDaFuncao = row["NomeDaFuncao"].ToString(),
+                                Atividades = new List<Atividade>()
+                            },
+                            Estabelecimento = new Estabelecimento()
+                            {
+                                UniqueKey = Guid.Parse(row["UKEstab"].ToString()),
+                                Descricao = row["Estabelecimento"].ToString()
+                            },
+                            Equipe = new Equipe()
+                            {
+                                NomeDaEquipe = row["NomeDaEquipe"].ToString()
+                            },
+                            Departamento = new Departamento()
+                            {
+                                Sigla = row["Sigla"].ToString()
+                            },
+                            ArquivoEmpregado = new List<ArquivoEmpregadoViewModel>()
+                        };
+
+                        al.ArquivoEmpregado = RetonarListaArquivoEmpregado(al.UniqueKey, UKEmpregado, al.Funcao.UniqueKey);
+
+                        if (!string.IsNullOrEmpty(row["Atividade"].ToString()))
+                        {
+                            al.Funcao.Atividades.Add(new Atividade()
+                            {
+                                Descricao = row["Atividade"].ToString()
+                            });
+                        }
+
+                    }
+                    else if (al.UniqueKey.ToString().Equals(row["UniqueKey"].ToString()))
+                    {
+                        if (!string.IsNullOrEmpty(row["Atividade"].ToString()))
+                        {
+                            al.Funcao.Atividades.Add(new Atividade()
+                            {
+                                Descricao = row["Atividade"].ToString()
+                            });
+                        }
+                    }
+                    else
+                    {
+                        lista.Add(al);
+
+                        al = new Alocacao()
+                        {
+                            UniqueKey = Guid.Parse(row["UniqueKey"].ToString()),
+                            DataInclusao = DateTime.Parse(row["DataInclusao"].ToString()),
+                            UsuarioInclusao = row["UsuarioInclusao"].ToString(),
+                            Contrato = new Contrato()
+                            {
+                                Numero = row["Contrato"].ToString()
+                            },
+                            Cargo = new Cargo()
+                            {
+                                NomeDoCargo = row["NomeDoCargo"].ToString()
+                            },
+                            Funcao = new Funcao()
+                            {
+                                UniqueKey = Guid.Parse(row["UKFuncao"].ToString()),
+                                NomeDaFuncao = row["NomeDaFuncao"].ToString(),
+                                Atividades = new List<Atividade>()
+                            },
+                            Estabelecimento = new Estabelecimento()
+                            {
+                                UniqueKey = Guid.Parse(row["UKEstab"].ToString()),
+                                Descricao = row["Estabelecimento"].ToString()
+                            },
+                            Equipe = new Equipe()
+                            {
+                                NomeDaEquipe = row["NomeDaEquipe"].ToString()
+                            },
+                            Departamento = new Departamento()
+                            {
+                                Sigla = row["Sigla"].ToString()
+                            },
+                            ArquivoEmpregado = new List<ArquivoEmpregadoViewModel>()
+                        };
+
+                        al.ArquivoEmpregado = RetonarListaArquivoEmpregado(al.UniqueKey, UKEmpregado, al.Funcao.UniqueKey);
+
+                        if (!string.IsNullOrEmpty(row["Atividade"].ToString()))
+                        {
+                            al.Funcao.Atividades.Add(new Atividade()
+                            {
+                                Descricao = row["Atividade"].ToString()
+                            });
+                        }
+
+                    }
+                } // Fim foreach
+
+                if (al != null)
+                {
+                    lista.Add(al);
+                }
+            }
+
+            return lista;
+        }
+
+        public List<ArquivoEmpregadoViewModel> RetonarListaArquivoEmpregado(Guid ukLocado, Guid ukEmpregado, Guid ukFuncao)
+        {
+            var lista = new List<ArquivoEmpregadoViewModel>();
+
+            var listaArquivoEmpregado = this.REL_ArquivoEmpregadoBusiness.Consulta
+                           .Where(x => x.UKLocacao == ukLocado
+                           && x.UKEmpregado == ukEmpregado
+                           && x.UKFuncao == ukFuncao && x.UsuarioExclusao == null).ToList();
+            if (listaArquivoEmpregado != null)
+            {
+                foreach (var arqemp in listaArquivoEmpregado)
+                {
+                    var arquivo = this.ArquivoBusiness.Consulta.SingleOrDefault(s => s.UniqueKey == arqemp.UKObjetoArquivo && arqemp.UsuarioExclusao == null);
+                    if (arquivo != null)
+                    {
+                        lista.Add(new ArquivoEmpregadoViewModel()
+                        {
+                            NomeLocal = arquivo.NomeLocal,
+                            NomeRemoto = arquivo.NomeRemoto,
+                            UniqueKey = arqemp.UniqueKey,
+                            UKLocacao = arqemp.UKLocacao,
+                            UKEmpregado = arqemp.UKEmpregado,
+                            UKFuncao = arqemp.UKFuncao
+                        });
+                    }
                 }
             }
 
